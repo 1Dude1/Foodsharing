9c0a149e4454fe660222f7f3d2cbb50373fab5bc
Degradin <42292046+Degradin@users.noreply.github.com>
2021-11-12 19:17:34 +0300

    Update process.php

--- frontend/scripts/process.php
+++ frontend/scripts/process.php
@@ -1,4 +1,9 @@
 <?php
     $username = $_POST['username'];
     $password = $_POST['password'];
+
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
+    $username = mysql_real($username);
+    $password = stripcslashes($password);
 ?>
